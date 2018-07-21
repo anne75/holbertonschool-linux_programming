e35502c3f9587d2f10d2058a722e834878cfe3f5
Anne <anne.cognet@holbertonschool.com>
2018-07-21 06:33:40 +0000

    Build on return value

--- 0x09-strace/print_syscall_return.c
+++ 0x09-strace/print_syscall_return.c
@@ -2,22 +2,36 @@
 #include "syscalls.h"
 
 /**
- * pre_action - print the syscall number
- * @regs: struct holding information collected by ptrace
+ * get_syscall_t - get syscall type
+ * @index: syscall number
+ * Return: syscall type
  */
-void pre_action(user_regs_t *regs)
+const syscall_t *get_syscall_t(size_t index)
 {
 	size_t arr_len, i;
 
 	arr_len = sizeof(syscalls_64_g) / sizeof(syscalls_64_g[0]);
 	for (i = 0; i < arr_len; ++i)
 	{
-		if (syscalls_64_g[i].nr == regs->orig_rax)
+		if (syscalls_64_g[i].nr == index)
 		{
-			printf("%s = ", syscalls_64_g[i].name);
-			break;
+			return (syscalls_64_g + i);
 		}
 	}
+	return (NULL);
+}
+
+/**
+ * pre_action - print the syscall number
+ * @regs: struct holding information collected by ptrace
+ */
+void pre_action(user_regs_t *regs)
+{
+	const syscall_t *syscall;
+
+	syscall = get_syscall_t(regs->orig_rax);
+	if (syscall)
+		printf("%s = ", syscall->name);
 	/* exit never returns here */
 	if (regs->orig_rax == 231)
 		printf("?\n");
@@ -29,10 +43,22 @@ void pre_action(user_regs_t *regs)
  */
 void post_action(user_regs_t *regs)
 {
+	const syscall_t *syscall;
+
+	syscall = get_syscall_t(regs->orig_rax);
 	/* execve is only seen on return */
 	if (regs->orig_rax == 59)
-		printf("%s = ", syscalls_64_g[regs->orig_rax].name);
-	printf("%lu\n", regs->rax);
+		printf("%s = ", syscall->name);
+	switch (syscall->ret)
+	{
+	case SSIZE_T :
+	case INT :
+		printf("%ld\n", regs->rax);
+		break;
+	case VOID_P :
+	default :
+		printf("%p\n", (void *)regs->rax);
+	}
 }
 
 /**
